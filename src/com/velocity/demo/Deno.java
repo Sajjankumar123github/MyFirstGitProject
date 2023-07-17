b4e7d71156195637ce2ca1bf789f575b61378d69
Sajjan <sajjanraut1998@gmail.com>
2023-07-17 14:25:55 +0530

    added codefor arraylist staging to local

---
+++ src/com/velocity/demo/Deno.java
@@ -0,0 +1,17 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+
+public class Deno {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add(25);
+		al.add("sajjan");
+		al.add(null);
+		al.add(null);
+		System.out.println(al);
+		
+	}
+
+}
